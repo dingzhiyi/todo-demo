929676f6e8bedf06fcc2ffb8795f4f815a59a93d
ding <dingzhiyi@hotmail.com>
2017-01-05 14:39:51 +0900

    add mongoose-query

---
+++ playground/mongoose-query.js
@@ -0,0 +1,30 @@
+const {ObjectID} = require(mongodb);
+
+var {mongoose} = require('./../server/db/mongoose');
+var {Todo} = require('./../server/models/todo');
+
+var id = "586d2d83bc68ee37fcdf891e";
+
+//find会返回所有符合搜索条件的条目，
+//findOne只会返回头一个，适合用来搜索除了Id以外的所有条件
+//findById用来搜索ID效果最佳
+
+
+Todo.find({
+  _id:id
+}).then((result)=>{
+  console.log(JSON.stringify(result,undefined,2));
+});
+
+
+Todo.findOne({
+  _id:id
+}).then((result)=>{
+  console.log(JSON.stringify(result,undefined,2));
+});
+
+
+
+Todo.findById(id).then((result)=>{
+  console.log(JSON.stringify(result,undefined,2));
+});
