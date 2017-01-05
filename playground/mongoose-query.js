be98e47765ee3e5aff2207eeebe8082598918363
ding <dingzhiyi@hotmail.com>
2017-01-05 15:26:45 +0900

    get added

--- playground/mongoose-query.js
+++ playground/mongoose-query.js
@@ -1,4 +1,4 @@
-const {ObjectID} = require(mongodb);
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./../server/db/mongoose');
 var {Todo} = require('./../server/models/todo');
@@ -8,6 +8,10 @@ var id = "586d2d83bc68ee37fcdf891e";
 //find会返回所有符合搜索条件的条目，
 //findOne只会返回头一个，适合用来搜索除了Id以外的所有条件
 //findById用来搜索ID效果最佳
+
+if(!ObjectID.isValid(id)){
+  return console.log("invalid user ID");
+}
 
 
 Todo.find({
@@ -22,7 +26,6 @@ Todo.findOne({
 }).then((result)=>{
   console.log(JSON.stringify(result,undefined,2));
 });
-
 
 
 Todo.findById(id).then((result)=>{
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -27,6 +28,25 @@ app.get('/todos',(req,res)=>{
   },(e)=>{
     res.status(400).send(e);
   })
+});
+
+
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send('Invalid ID');
+  }
+
+  Todo.findById(String(id)).then((result)=>{
+    if(!result){
+      return res.status(404).send('No results');
+    }
+    res.status(200).send({result});
+
+  }).catch((e)=>{
+    res.status(404).send('errors happened');
+  })
+
 });
 
 
